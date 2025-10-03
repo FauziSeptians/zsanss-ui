773ee1b61356ac49286848e44bf2f7fb19220336
Muhammad Fauzi Septiana Putra <114633811+FauziSeptians@users.noreply.github.com>
2025-10-03 20:55:59 +0700

    fix : typography (#5)
    * feat: tab menu
    
    * feat: tab menu
    
    * fix: typography
    
    ---------
    
    Co-authored-by: muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>

--- lib/components/Typography/Text.tsx
+++ lib/components/Typography/Text.tsx
@@ -1,50 +1,53 @@
 import React from "react";
 import { TextTypes } from "@/types/main";
 import classNames from "@/utils/classNames";
-import {STATUS, getTypeClass} from "@/constants/typograpgy"
+import { STATUS, getTypeClass } from "@/constants/typograpgy";
 
 interface ExtendedTextTypes extends TextTypes {
-   highlightText?: string;
+  highlightText?: string;
 }
 
 const highlightContent = (
-   text: React.ReactNode,
-   highlight: string
+  text: React.ReactNode,
+  highlight: string
 ): React.ReactNode => {
-   if (!highlight || typeof text !== "string") {
-      return text;
-   }
+  if (!highlight || typeof text !== "string") {
+    return text;
+  }
 
-   const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+  const parts = text.split(new RegExp(`(${highlight})`, "gi"));
 
-   return parts.map((part, index) =>
-      part.toLowerCase() === highlight.toLowerCase() ? (
-         <mark key={index} className="rounded">
-            {part}
-         </mark>
-      ) : (
-         part
-      )
-   );
+  return parts.map((part, index) =>
+    part.toLowerCase() === highlight.toLowerCase() ? (
+      <mark key={index} className="rounded">
+        {part}
+      </mark>
+    ) : (
+      part
+    )
+  );
 };
 
 export default function Text(props: ExtendedTextTypes) {
-   const { highlightText, children, ...restProps } = props;
+  const { highlightText, children, ...restProps } = props;
 
-   const classes = classNames(
-      restProps?.bold && STATUS.BOLD,
-      restProps?.italic && STATUS.ITALIC,
-      restProps?.underline && STATUS.UNDERLINE,
-      getTypeClass(restProps?.type),
-      "text-sm",
-      restProps?.className
-   );
+  const classes = classNames(
+    restProps?.bold && STATUS.BOLD,
+    restProps?.italic && STATUS.ITALIC,
+    restProps?.underline && STATUS.UNDERLINE,
+    getTypeClass(restProps?.type),
+    "text-sm ",
+    "tracking-wider",
+    "leading-relaxed",
+    "antialiased",
+    restProps?.className
+  );
 
-   if (highlightText && highlightText.trim()) {
-      return (
-         <p className={classes}>{highlightContent(children, highlightText)}</p>
-      );
-   }
+  if (highlightText && highlightText.trim()) {
+    return (
+      <p className={classes}>{highlightContent(children, highlightText)}</p>
+    );
+  }
 
-   return <p className={classes}>{children}</p>;
+  return <p className={classes}>{children}</p>;
 }

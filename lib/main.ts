4a1f88744686dbf9a0c2ce323d79a4140f25bee9
muhammad.putra.ext <muhammad.putra.ext@bcadigital.co.id>
2025-10-03 18:03:48 +0700

    feat: input dropdown

--- lib/main.ts
+++ lib/main.ts
@@ -7,6 +7,8 @@ export { default as Card } from "./components/Card";
 export { default as Description } from "./components/Card/Description";
 export { default as Image } from "./components/Image";
 export { default as Button } from "./components/Button";
+export { default as DropdownWrapper } from "./components/Dropdown/index";
+export { default as Dropdown } from "./components/Dropdown/Dropdown";
 
 export * from "./types/main";
 export * from "./utils/main";
--- lib/types/main.ts
+++ lib/types/main.ts
@@ -2,3 +2,4 @@ export type { TextTypes } from "./text";
 export type { LayoutTypes, ObjectSidebar } from "./layout";
 export type { CardTypes } from "./card";
 export type { ButtonTypes } from "./button";
+export type { DropdownTypes, InputDropdownTypes, itemTypes } from "./dropdown";
